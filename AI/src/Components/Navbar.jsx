eba9134629852680284ff525edcddc94c31950dc
unknown <sohamvashists8@gmail.com>
2024-11-29 23:19:12 +0530

    New Initialize

---
+++ AI/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <header className=''>
+        <div className="searchbar">
+
+        </div>
+
+        <div className="Logo">
+<h1>TRYLY</h1>
+        </div>
+
+        <div className="Account">
+
+        </div>
+
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
