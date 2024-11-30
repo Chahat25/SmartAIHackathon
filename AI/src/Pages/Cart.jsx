36626cea7b99feda40cd519114393ffce3cd706e
unknown <sohamvashists8@gmail.com>
2024-12-01 01:27:42 +0530

    New Initialize

--- AI/src/Pages/Cart.jsx
+++ AI/src/Pages/Cart.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
+import Header from '../Components/Header'
 
 function Cart() {
   return (
     <div>
-      
+      <Navbar />
+      <Header />
     </div>
   )
 }
