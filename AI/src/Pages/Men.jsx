aef5fd275e1bf74b7707d1282f1477de9f8766c3
Woodocarlo <nischalsharma2037@gmail.com>
2024-12-01 15:20:03 +0530

    aisehi

--- AI/src/Pages/Men.jsx
+++ AI/src/Pages/Men.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
-import Products from '../Components/Products'
+import Products from '../Components/Products_man'
 
 function Men() {
   return (
